da19ad09f4250d5abbc810c6d8b34581b46630e5
threader <40972803+localthreader@users.noreply.github.com>
2021-03-08 12:23:54 -0300

    Removendo bin do repositório, vazou o token

--- bin/main/sql/inserts/cursos.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Curso VALUES (1, 'Ensino Médio');
-INSERT INTO Curso VALUES (2, 'Técnico em Desenvolvimento de Sistemas');
-INSERT INTO Curso VALUES (3, 'Técnico em Edificações');
-INSERT INTO Curso VALUES (4, 'Técnico em Geodésia e Cartografia');
-INSERT INTO Curso VALUES (5, 'Técnico em Mecânica');
-INSERT INTO Curso VALUES (6, 'Técnico em Qualidade');
-INSERT INTO Curso VALUES (7, 'Técnico em Enfermagem');
--- bin/main/sql/inserts/periodos.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Periodo VALUES (1, 'Diurno');
-INSERT INTO Periodo VALUES (2, 'Noturno');
